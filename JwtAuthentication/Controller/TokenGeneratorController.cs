2005d03ed56590938b73622897a98d70cc4fe0e6
aakash.karve <aakash.karve@gmail.com>
2019-02-16 14:29:18 +0530

    Initial Commit

---
+++ JwtAuthentication/Controller/TokenGeneratorController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace JwtAuthentication.Controller1
+{
+    public class TokenGeneratorController : Controller
+    {
+        [HttpGet("api/[controller]/[action]")]
+        public IActionResult RequestToken()
+        {
+            var claims = new[]
+        {
+            new Claim("TestClaim","TestValue"),
+            new Claim(ClaimTypes.Role,"Manager")
+        };
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("TestKey1234567845454545454545454545454"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "yourdomain.com",
+                audience: "yourdomain.com",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(20),
+                signingCredentials: creds);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
+
+        // NOTE:  // Need to send token in Authorization Header as "Bearer TokenValue"
+        [HttpGet("api/[controller]/IsUserValid"), Authorize]
+        public IActionResult IsUSerValid()
+        {
+            var userClaims = User.Claims;
+            return Ok(true);
+        }
+
+        // NOTE:  // Need to send token in Authorization Header as "Bearer TokenValue"
+        [HttpGet("api/[controller]/IsManager"), Authorize(Policy = "ManagerPolicy")]
+        public IActionResult IsManager()
+        {
+            var userClaims = User.Claims;
+            return Ok(true);
+        }
+    }
+}
---
+++ JwtAuthentication/Startup.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+
+namespace JwtAuthentication
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var key = System.Text.Encoding.ASCII.GetBytes("TestKey1234567845454545454545454545454");
+            services.AddAuthentication(options =>
+           {
+               options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+               options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+           })
+           .AddJwtBearer(options =>
+           {
+               options.SaveToken = false;
+               options.RequireHttpsMetadata = false;
+               options.TokenValidationParameters = new TokenValidationParameters()
+               {
+                   ValidAudience = "yourdomain.com",
+                   ValidIssuer = "yourdomain.com",
+                   ClockSkew = TimeSpan.Zero,
+                   ValidateAudience = true,
+                   ValidateIssuer = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+                   IssuerSigningKey = new SymmetricSecurityKey(key)
+               };
+
+               options.Events = new JwtBearerEvents()
+               {
+                   // called when token validation failed due to validation parameters
+                   OnAuthenticationFailed = async (context) =>
+                   {
+                       int x = context.Response.StatusCode;
+                   },
+
+                   //called when request arrives
+                   OnMessageReceived = async (context) =>
+                   {
+                       int x = context.Response.StatusCode;
+                   },
+
+                   // called when response is 401 Unauthorized
+                   OnChallenge = async (context) =>
+                   {
+                       int x = context.Response.StatusCode;
+                   },
+
+                   // Called after token is validated
+                   OnTokenValidated = async (context) =>
+                   {
+                       int x = context.Response.StatusCode;
+                   },
+               };
+           });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ManagerPolicy", policyBuilder =>
+                {
+                    policyBuilder.RequireRole("Manager");
+                });
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+          {
+              try
+              {
+                  await next();
+              }
+              catch (Exception ex)
+              {
+                  await context.Response.WriteAsync(ex.Message);
+              }
+          });
+
+            app.UseStatusCodePages(async statusCodeContext =>
+           {
+               if (statusCodeContext.HttpContext.Response.StatusCode == 401)
+               {
+                   await statusCodeContext.HttpContext.Response.WriteAsync("UnAuthorized !!!");
+               }
+
+               if (statusCodeContext.HttpContext.Response.StatusCode == 403)
+               {
+                   await statusCodeContext.HttpContext.Response.WriteAsync("Forbidden !!!");
+               }
+           });
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
